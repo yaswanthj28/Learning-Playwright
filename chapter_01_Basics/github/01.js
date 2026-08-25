c2d1d6969f9dfd8bb9ef1a2ae9214f5f169719b6
yaswanthj28 <yaswanthj28@gmail.com>
2026-08-25 21:38:18 +0530

    Commited new changes

---
+++ chapter_01_Basics/github/01.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+//Creating new repositary
+
+//echo "# gitRepositary name" >> README.md
+//git init
+//git add .
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin <git respo link >
+//git push -u origin main
+
+
+//Pushing code to existing git respositary
+
+// git add foldername/filename to stage the file for commit
+// git commit -m "commit message" to commit the file
+// git push -u origin main to push the file to the remote repository
+// git push origin main to push the file to the remote repository if already pushed once
